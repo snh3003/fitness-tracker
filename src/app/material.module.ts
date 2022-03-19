d972ec9ca1adc494f3036401200410c76b85adee
snh3003 <shahsama542@gmail.com>
2022-03-19 17:05:24 +0530

    Added material form field and material input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatButtonModule, MatInputModule, MatIconModule],
-  exports: [MatButtonModule, MatInputModule, MatIconModule],
+  imports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatInputModule, MatIconModule, MatFormFieldModule],
 })
 export class MaterialModule {}
