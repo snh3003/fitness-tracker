a4bbf4daeb3819d1f170d21445ac3dc0b9ee64de
snh3003 <shahsama542@gmail.com>
2022-06-28 22:32:18 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD0Z-9-C3m_n65NBAuiE82B1Yg2dZG8PI4',
+    authDomain: 'fitness-tracker-f9136.firebaseapp.com',
+    projectId: 'fitness-tracker-f9136',
+    storageBucket: 'fitness-tracker-f9136.appspot.com',
+    messagingSenderId: '16722051077',
+    appId: '1:16722051077:web:f587e9f301620777eb5c6c',
+    measurementId: 'G-K0WXPYVV12',
+  },
 };
